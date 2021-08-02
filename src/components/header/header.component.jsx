46f999ed7cc8b48190eaec53ec30a4ec2c86f044
ይመስገን ሞርካ <34184655+Dochomorka@users.noreply.github.com>
2021-08-02 15:12:37 +0300

    added cartIcon and CartDropdown component to our application , also implemented cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.scss';
 import {connect} from 'react-redux';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils';
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.componet';
+
+const Header = ({ currentUser, hidden }) => (
    <div className='header'>
       <Link to="/" className='logo-container'>
          <Logo className='logo' />
@@ -15,12 +17,17 @@ const Header = ({ currentUser }) => (
          <Link className='option' to='/shop'>CONTACT</Link>
          {currentUser ? <div className='option' onClick={() => auth.signOut()}>SGIN OUT</div>
             : <Link className='option' to="/signin">SIGN IN</Link>}
+            <CartIcon />
       </div>
+      {
+         hidden ? null : <CartDropdown/> 
+      }
    </div> 
-)
+);
 
-const mapStateToPorps = (state) => ({
-    currentUser: state.user.currentUser,
+const mapStateToPorps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser: currentUser,
+    hidden: hidden
 });
 
 export default connect(mapStateToPorps)(Header);
