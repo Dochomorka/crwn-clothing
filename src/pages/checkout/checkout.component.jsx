7c716d382ec3454404afb40932d3191abacdee69
ይመስገን ሞርካ <34184655+Dochomorka@users.noreply.github.com>
2021-08-05 09:43:53 +0300

    added addItem and removeItem functionality to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,32 +8,32 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
-const CheckoutPage = ({cartItems, total}) => (
-<div className="checkout-page">
-    <div className="checkout-header">
-        <div className="header-block">
-        <span>Product</span>
+const CheckoutPage = ({ cartItems, total }) => (
+    <div className="checkout-page">
+        <div className="checkout-header">
+            <div className="header-block">
+                <span>Product</span>
+            </div>
+            <div className="header-block">
+                <span>Description</span>
+            </div>
+            <div className="header-block">
+                <span>Quantity</span>
+            </div>
+            <div className="header-block">
+                <span>Price</span>
+            </div>
+            <div className="header-block">
+                <span>Remove</span>
+            </div>
         </div>
-        <div className="header-block">
-        <span>Description</span>
-        </div>
-        <div className="header-block">
-        <span>Quantity</span>
-        </div>
-        <div className="header-block">
-        <span>Price</span>
-        </div>
-        <div className="header-block">
-        <span>Remove</span>
+        {
+            cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
+        }
+        <div className="total">
+            <span>Total : ${total}</span>
         </div>
     </div>
-    {
-        cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>))
-    }
-    <div className="total">
-        <span>Total : ${total}</span>
-    </div>
-</div>
 )
 
 const mapStateToProps = createStructuredSelector({
